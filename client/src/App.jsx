1968d1a91295111ced760fd786cfc2c52c3e46db
KarthikSapaliga <im.karthiksapaliga@gmail.com>
2025-03-14 22:22:15 +0530

    Added BrowserRoutes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Auth from "./pages/auth/Auth";
+import Chat from "./pages/chat/Chat";
+import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Navigate to="/auth" replace />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
